aafd551cc1ced132b34eb796f07580800d493a7b
madrien-dev <adrienmeunier85@gmail.com>
2021-06-14 14:45:18 +0200

    new branch article

---
+++ e_commerce/lib/screen/article.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArticleScreen extends StatefulWidget {
+  @override
+  _ArticleScreenState createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends State<ArticleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
